4c676a85f734f085a10cc086056adc31e2fc8903
szymondlugolecki <szymon.dlugolecki77@gmail.com>
2024-06-20 13:30:34 +0200

    chore: Update ReplyPost component to preserve line breaks in content

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+## Tech stack
+
+```bash
+# core
+next.js 14
+typescript
+tailwindcss
+shadcn
+cloudflare pages
+
+```
+
+---
+
+# TODO
+[  ] Add fancy hidden features
+
+---
+
 ## Getting Started
 
 First, run the development server:
--- src/lib/posts/contact.ts
+++ src/lib/posts/contact.ts
@@ -18,29 +18,53 @@ const contactPosts: PostData[] = [
     category: "contact",
     type: "child",
     content:
-      "Please note that all of my repositories are private, but if you're interested in hiring me, I'm willing to share some of them with you. Below are some of the website that I've made:",
+      "Please note that all of my repositories (besides this portfolio) are private. If you're interested I can share the source code or give admin access to websites of choice.",
     date: new Date(1673534895046),
   },
   {
     category: "contact",
     type: "child",
-    content: "ImageBlade - image hosting and optimization SaaS",
-    link: "https://imageblade.com",
+    content: "Below are some of the projects I've been working on:",
     date: new Date(1673534895046),
   },
   {
     category: "contact",
     type: "child",
     content:
-      "Twoje ALDO - ecommerce website for a local business in Poland (for selected customers only)",
+      "Twoje ALDO - ecommerce PWA for a local business in Poland\nStack: SvelteKit & Typescript & Tailwind.css\nStatus: Testing in private beta",
     link: "https://twojealdo.pl",
     date: new Date(1673534895046),
   },
   {
     category: "contact",
     type: "child",
-    content: "Interhome - simple static website for a local business",
+    content:
+      "Interhome - simple static website for a local business\nStack: SvelteKit & Typescript & Tailwind.css\nStatus: In production",
     link: "https://interhome.com.pl",
+    date: new Date(1673534895046),
+  },
+  {
+    category: "contact",
+    type: "child",
+    content:
+      "Lab - personal medical blog with study breakdowns and articles\nStack: Next.js 14 & Typescript & Tailwind.css\nStatus: Early development",
+    link: "https://lab.dlugolecki.me",
+    date: new Date(1673534895046),
+  },
+  {
+    category: "contact",
+    type: "child",
+    content:
+      "Personal portfolio (this website)\nStack: Next.js 14 & Typescript & Tailwind.css\nStatus: In production",
+    link: "https://dlugolecki.me",
+    date: new Date(1673534895046),
+  },
+  {
+    category: "contact",
+    type: "child",
+    content:
+      "ImageBlade - image hosting and optimization SaaS\nStack: SvelteKit & Typescript & Tailwind.css\nStatus: Early development",
+    link: "https://imageblade.com",
     date: new Date(1673534895046),
   },
 ];
